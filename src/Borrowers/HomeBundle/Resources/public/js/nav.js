13a9965f66abf4ba7b6cdbe4c25b21c878c4af0b
Ron Balthazor <ron.balthazor@gmail.com>
2013-02-13 11:17:35 -0500

    cleanup

--- src/Borrowers/HomeBundle/Resources/public/js/nav.js
+++ src/Borrowers/HomeBundle/Resources/public/js/nav.js
@@ -1,33 +1,33 @@
 comment = "Borrowers and Lenders";
 a1 = new Image(150,58);
-a1.src = "/borrowers/current_issue_off.jpg";
+a1.src = "/bundles/borrowershome/img/current_issue_off.jpg";
 a2 = new Image(150,58);
-a2.src = "/borrowers/current_issue_on.jpg";
+a2.src = "/bundles/borrowershome/img/current_issue_on.jpg";
 
 b1 = new Image(150,54);
-b1.src = "/borrowers/previous_issue_off.jpg";
+b1.src = "/bundles/borrowershome/img/previous_issue_off.jpg";
 b2 = new Image(150,54);
-b2.src = "/borrowers/previous_issue_on.jpg";
+b2.src = "/bundles/borrowershome/img/previous_issue_on.jpg";
 
 c1 = new Image(150,36);
-c1.src = "/borrowers/archive-off.jpg";
+c1.src = "/bundles/borrowershome/img/archive-off.jpg";
 c2 = new Image(150,36);
-c2.src = "/borrowers/archive-on.jpg";
+c2.src = "/bundles/borrowershome/img/archive-on.jpg";
 
 d1 = new Image(150,36);
-d1.src = "/borrowers/b4-off-book-d2.jpg";
+d1.src = "/bundles/borrowershome/img/b4-off-book-d2.jpg";
 d2 = new Image(150,36);
-d2.src = "/borrowers/b4-on-book-d2.jpg";
+d2.src = "/bundles/borrowershome/img/b4-on-book-d2.jpg";
 
 e1 = new Image(150,36);
-e1.src = "/borrowers/b5-off-book-d2.jpg";
+e1.src = "/bundles/borrowershome/img/b5-off-book-d2.jpg";
 e2 = new Image(150,36);
-e2.src = "/borrowers/b5-on-book-d2.jpg";
+e2.src = "/bundles/borrowershome/img/b5-on-book-d2.jpg";
 
 f1 = new Image(150,36);
-f1.src = "/borrowers/b6-off-book-d2.jpg";
+f1.src = "/bundles/borrowershome/img/b6-off-book-d2.jpg";
 f2 = new Image(150,36);
-f2.src = "/borrowers/b6-on-book-d2.jpg";
+f2.src = "/bundles/borrowershome/img/b6-on-book-d2.jpg";
 
 //image swapping function:
 function imageSwap(imgDocID, imgObjName, comment) {
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit7d52cb2caac2516895c7bf5c5801b4a6::getLoader();
+return ComposerAutoloaderInit14867864451471fa2c3640aa064b18df::getLoader();
