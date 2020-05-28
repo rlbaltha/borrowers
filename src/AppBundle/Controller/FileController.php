a179dd81084e9393f40514ebd867f4a38efbdab7
Ron Balthazor <rlbaltha@uga.edu>
2020-05-28 15:28:34 -0400

    upload fix test

--- src/AppBundle/Controller/FileController.php
+++ src/AppBundle/Controller/FileController.php
@@ -46,7 +46,12 @@ class FileController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $file = $em->getRepository('AppBundle:File')->find($id);
-        $path = __DIR__.'/../../../borrowers_docs/'.$file->getPath();
+        if (substr( $file->getPath(), 0, 7 ) === "uploads") {
+            $path = __DIR__.'/../../../borrowers_docs/'.$file->getPath();
+        }
+        else {
+            $path = __DIR__.'/../../../borrowers_docs/uploads/files/'.$file->getIssue()->getIssue().'/'.$file->getPath();
+        }
 
         if (!$file) {
             throw $this->createNotFoundException('Unable to find File entity.');
@@ -268,8 +273,13 @@ class FileController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $file = $em->getRepository('AppBundle:File')->find($id);
-        $path = __DIR__.'/../../../borrowers_docs/'.$file->getPath();
-//        $path = __DIR__.'/../../../../borrowers_docs/uploads/files/'.$file->getIssue()->getIssue().'/'.$file->getPath();
+        if (substr( $file->getPath(), 0, 7 ) === "uploads") {
+            $path = __DIR__.'/../../../borrowers_docs/'.$file->getPath();
+        }
+        else {
+            $path = __DIR__.'/../../../borrowers_docs/uploads/files/'.$file->getIssue()->getIssue().'/'.$file->getPath();
+        }
+
 
         if (!$file) {
             throw $this->createNotFoundException('Unable to find File entity.');
